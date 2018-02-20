f24a4fa7d8c7ec123036841906f0ae5cfffe732f
Christina Bertram <Jamerican92grl@MacBook-Pro.home>
2018-02-20 02:04:45 -0500

    Added 10 Products to the database

--- bamazon.sql
+++ bamazon.sql
@@ -10,3 +10,33 @@ CREATE TABLE products (
   stock_quantity INT(11),
   PRIMARY KEY(item_id)
 );
+
+INSERT INTO products (product_name, department_name, price, stock_quantity) 
+VALUES ("UO Jeanne Lace-Up Back Mini Dress", "Women's Clothes", 49, 80);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity) 
+VALUES ("UO Marie Prairie Wrap Jumpsuit", "Women's Clothes", 79, 60);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity) 
+VALUES ("Lazy Oaf Blue Stripe Velour Crew Neck Sweatshirt", "Men's Clothes", 90, 65);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity) 
+VALUES ("G-Star Elwood Vintage Aloha Palm Slim Jean", "Men's Clothes", 170, 50);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity) 
+VALUES ("Sloane Seamed Suede Ankle Boot", "Womens's Shoes", 89, 90);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity) 
+VALUES ("Vans Classic Slip-On Sneaker", "Mens's Shoes", 50, 100);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity) 
+VALUES ("Mura Bench", "Furniture", 229, 75);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity) 
+VALUES ("Dempsey Floor Lamp", "Furniture", 89, 100);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity) 
+VALUES ("Mionix Wei Color Backlit Keyboard", "Tech", 164, 50);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity) 
+VALUES ("Polaroid Originals Refurbished Silver OneStep Instant Camera", "Tech", 170, 50);
